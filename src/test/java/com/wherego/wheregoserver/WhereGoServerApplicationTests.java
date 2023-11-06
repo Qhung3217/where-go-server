542f75a61fc529b075f61397688a594a3b875334
Qhung <quanghung165200@gmail.com>
2023-11-06 15:27:38 +0700

    initial commit

---
+++ src/test/java/com/wherego/wheregoserver/WhereGoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wherego.wheregoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereGoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

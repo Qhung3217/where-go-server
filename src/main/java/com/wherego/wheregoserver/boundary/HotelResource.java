4f9fa51fe7c882a78f367806d5026d7f67f7cc72
Qhung <quanghung165200@gmail.com>
2023-11-09 19:51:01 +0700

    [Done] hotel select all

--- src/main/java/com/wherego/wheregoserver/boundary/HotelResource.java
+++ src/main/java/com/wherego/wheregoserver/boundary/HotelResource.java
@@ -1,6 +1,7 @@
 package com.wherego.wheregoserver.boundary;
 
 import com.wherego.wheregoserver.dto.hotel.HotelFilterInforDto;
+import com.wherego.wheregoserver.dto.hotel.SimpleHotelDto;
 import com.wherego.wheregoserver.service.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,14 +10,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value="/api/hotel")
 public class HotelResource {
     @Autowired
     private HotelService hotelService;
-    @GetMapping(value = "/filter-infor", produces = "application/json")
+
+    @GetMapping
+    public ResponseEntity<List<SimpleHotelDto>> getAll(){
+        return new ResponseEntity<List<SimpleHotelDto>>(hotelService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/filter-infor")
     public ResponseEntity<HotelFilterInforDto> getHotelFilterInfor(){
         return new ResponseEntity<HotelFilterInforDto>(hotelService.getHotelFilterInfor(),
         HttpStatus.OK);
     }
+
+
 }
--- src/main/java/com/wherego/wheregoserver/service/impl/HotelServiceImpl.java
+++ src/main/java/com/wherego/wheregoserver/service/impl/HotelServiceImpl.java
@@ -1,19 +1,35 @@
 package com.wherego.wheregoserver.service.impl;
 
 import com.wherego.wheregoserver.dto.hotel.HotelFilterInforDto;
+import com.wherego.wheregoserver.dto.hotel.SimpleHotelDto;
+import com.wherego.wheregoserver.mapper.HotelMapper;
 import com.wherego.wheregoserver.respository.HotelRepository;
 import com.wherego.wheregoserver.service.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class HotelServiceImpl implements HotelService {
     @Autowired
     private HotelRepository hotelRepository;
+
+    @Autowired
+    private HotelMapper hotelMapper;
     @Override
     public HotelFilterInforDto getHotelFilterInfor() {
         return new HotelFilterInforDto(hotelRepository.getRoomTypes(),
                 hotelRepository.getRoomFeatures(),
                 hotelRepository.getPropertyAmenities());
+    }
+
+    @Override
+    public List<SimpleHotelDto> getAll() {
+        return hotelRepository.getAll()
+                .stream()
+                .map(hotel -> hotelMapper.toSimpleHotelDto(hotel))
+                .collect(Collectors.toList());
     }
 }

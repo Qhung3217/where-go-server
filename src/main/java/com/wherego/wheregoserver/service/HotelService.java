db0c0e7100c39cb7fbc637657cbdd9f507bf885b
Qhung <quanghung165200@gmail.com>
2023-11-09 13:34:36 +0700

    Move HotelFilterInforDto to package dto.hotel

--- src/main/java/com/wherego/wheregoserver/service/HotelService.java
+++ src/main/java/com/wherego/wheregoserver/service/HotelService.java
@@ -1,9 +1,10 @@
 package com.wherego.wheregoserver.service;
 
-import com.wherego.wheregoserver.dto.HotelFilterInforDto;
+import com.wherego.wheregoserver.dto.hotel.HotelFilterInforDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface HotelService {
     HotelFilterInforDto getHotelFilterInfor();
+
 }

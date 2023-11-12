03b7035faa807da67b5deacbd8c26df889657b26
Qhung <quanghung165200@gmail.com>
2023-11-12 15:57:45 +0700

    Implement function toDetailPlaceDto

--- src/main/java/com/wherego/wheregoserver/mapper/PlaceMapper.java
+++ src/main/java/com/wherego/wheregoserver/mapper/PlaceMapper.java
@@ -1,7 +1,10 @@
 package com.wherego.wheregoserver.mapper;
 
+import com.wherego.wheregoserver.dto.ReviewDetailDto;
+import com.wherego.wheregoserver.dto.place.DetailPlaceDto;
 import com.wherego.wheregoserver.dto.place.SimplePlaceDto;
 import com.wherego.wheregoserver.repository.entity.Place;
+import com.wherego.wheregoserver.repository.entity.PlaceGallery;
 import com.wherego.wheregoserver.repository.entity.PlaceReview;
 import com.wherego.wheregoserver.repository.entity.PlaceType;
 import com.wherego.wheregoserver.utils.MapperUtils;
@@ -19,6 +22,14 @@ public interface PlaceMapper {
     @Mapping(target="totalRating", source="reviews", qualifiedByName ="getTotalRating")
     public SimplePlaceDto toSimplePlaceDto(Place place);
 
+    @Mapping(target="districtName", source="district.name")
+    @Mapping(target="types", source="types", qualifiedByName = "getTypeNames")
+    @Mapping(target="averageRating", source="reviews", qualifiedByName = "getAverageRating")
+    @Mapping(target="totalRating", source="reviews", qualifiedByName ="getTotalRating")
+    @Mapping(target="galleries", source="galleries", qualifiedByName="getGalleries")
+    @Mapping(target="reviews", source="reviews", qualifiedByName="getReviews")
+    public DetailPlaceDto toDetailPlaceDto(Place place);
+
     @Named("getTypeNames")
     public static List<String> getTypeNames(Set<PlaceType> types){
         return MapperUtils.arrayToListString(types, "getType");
@@ -33,4 +44,15 @@ public interface PlaceMapper {
     public static int getTotalRating(Set<PlaceReview> reviews){
         return reviews.size();
     }
+
+    @Named("getGalleries")
+    public static List<String> getGalleries(Set<PlaceGallery> galleries){
+        return MapperUtils.arrayToListString(galleries, "getImage");
+    }
+
+    @Named("getReviews")
+    public static Set<ReviewDetailDto> getReviews(Set<PlaceReview> reviews){
+        return MapperUtils.objectReviewToReviewDetail(reviews);
+    }
+
 }

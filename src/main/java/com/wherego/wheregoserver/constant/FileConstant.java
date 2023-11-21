e3a62bd91ff41f9e69778d6b86a9a4e160afd7df
Qhung <quanghung165200@gmail.com>
2023-11-21 16:11:24 +0700

    Config/Only support jpg and png file

--- src/main/java/com/wherego/wheregoserver/constant/FileConstant.java
+++ src/main/java/com/wherego/wheregoserver/constant/FileConstant.java
@@ -5,5 +5,5 @@ import java.util.List;
 public class FileConstant {
     public static final String DEFAULT_IMAGE = "default.jpg";
     public static final String UPLOAD_DIR = "uploads";
-    public static final List<String> ALLOWED_IMAGE_TYPES = List.of("image/jpeg", "image/png", "image/gif");
+    public static final List<String> ALLOWED_IMAGE_TYPES = List.of("image/jpeg", "image/png");
 }
